2ce7884b5fa72cb319ddfab34963a0080e8423fb
wanglei <wn0001211@163.com>
2020-12-24 20:26:29 +0800

    二次修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,72 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+
+    component: () => import('../views/Login.vue')
+  }, {
+
+    path: '/home',
+    name: 'Home',
+    redirect: '/index',
+    meta: { title: '首页' },
+    component: () => import('../views/Home/Home.vue'),
+    children: [
+      {
+        path: '/index',
+        name: 'Index',
+        meta: { title: '首页' },
+        component: () => import('../views/Home/Index/index.vue')
+      },
+      {
+        path: '/wms',
+        name: 'Wms',
+        meta: { title: '信息管理' },
+        component: () => import('../views/Home/wms/index.vue'),
+        children: [
+          {
+            path: '/wms/list',
+            name: 'wmsList',
+            meta: { title: '选项一' },
+            component: () => import('../views/Home/wms/list.vue')
+          }, {
+            path: '/wms/info',
+            name: 'wmsInfo',
+            meta: { title: '选项二' },
+            component: () => import('../views/Home/wms/info.vue')
+          }
+        ]
+      },
+      {
+        path: '/stats',
+        name: 'Stats',
+        meta: { title: '数据统计' },
+        component: () => import('../views/Home/stats/index.vue')
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+//路由拦截
+router.beforeEach(function (to, from, next) {
+  if (!sessionStorage.getItem('username')) {
+    if (to.path != '/login') {
+      next('/login')
+    }
+  }
+  next()
+})
 export default router
