af8b41349e6076ee1b4aa59f2f7d3ee414ce1af4
wanglei <wn0001211@163.com>
2020-12-24 20:25:55 +0800

    一次修改

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+//axios全局引入
+Vue.prototype.$http = Axios;
+Axios.defaults.baseURL = 'http://127.0.0.1:8089'
+//默认路径
 
 new Vue({
   router,
